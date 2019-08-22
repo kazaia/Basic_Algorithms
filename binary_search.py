8af744ad9708e72e89cf0712f5dd52d5eb44932c
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-08-22 14:11:59 +0100

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,4 +1,11 @@
 # Binary search
 
 
+# Given a sorted array of integers, and a target value, find the index of the target value in the array. If the target value is not present in the array, return -1.
+
+
+
+
+
+
 
