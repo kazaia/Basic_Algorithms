d71f0a7613a7bbe4ecf9a745c011ba7beec256b8
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-10-11 09:06:12 +0100

    Update bubble_sort_algorithm.py

--- sorting algorithms/bubble_sort_algorithm.py
+++ sorting algorithms/bubble_sort_algorithm.py
@@ -1,6 +1,15 @@
 # Bubble sort algorithm
 
+def bubble_sort(input):
+    for i in range(len(input) - 1):
+        for j in range(len(input) - 1):
+            if input[j] > input[j + 1]:
+                input[j], input[j + 1] = input[j + 1], input[j ]
 
+    return input
+
+l = [4, 5, 3, 1, 0, 8]
+print(bubble_sort(l))
 
 
 
